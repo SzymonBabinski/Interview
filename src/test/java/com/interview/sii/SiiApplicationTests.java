16a9b69151562054fdbdb6f1305b468188cf2360
Szymon <szymon.babinski@student.put.poznan.pl>
2021-06-18 20:36:56 +0200

    Initialize project

---
+++ src/test/java/com/interview/sii/SiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.sii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
